3682dd6fc0ed145a8cbc45d666dba5a31c3aaf1d
Edgardo cayo <edgardo.cayo.miranda@cftmail.cl>
2022-11-11 10:59:32 -0300

    subida el proyecto componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "inicio",
+    component: InicioComponent
+  },
+  {
+    path: "contacto",
+    component: ContactoComponent
+  },
+  {
+    path: "nosotros",
+    component: NosotrosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
+import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ContactoComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
